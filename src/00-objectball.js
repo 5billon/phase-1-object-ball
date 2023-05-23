7ef4da74c5dc22406d9f08f5e035c42514ea222f
Shanley Caswell <shanleycaswell@gmail.com>
2023-05-23 16:16:51 -0400

    more stuff

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,27 +1,66 @@
 function gameObject() {
-    let playersName = ['Alan Anderson', 'Reggie Evans', 'Brook Lopez', 'Mason Plumlee', 'Jason Terry']
-    home: {
-        teamName: ''
-        colors: ['']
-        players: [
-            playersName = {
-                number: '',
-                shoe:'',
-                points:'',
-                rebounds:'',
-                assists:'',
-                steals:'',
-                blocks:'',
-                slamDunks:''
+    return {
+        home: {
+            teamName: 'Brooklyn Nets',
+            colors: ['Black, White'],
+            players: {
+                "Alan Anderson": {
+                    number: "0",
+                    shoe: "16",
+                    points: "22",
+                    rebounds: "12",
+                    assists: "12",
+                    steals: "3",
+                    blocks: "1",
+                    slamdunks: "1"
+
+                },
+                "Reggie Evans": {
+                    number: "30",
+                    shoe: "14",
+                    points: "12",
+                    rebounds: "12",
+                    assists: "12",
+                    steals: "12",
+                    blocks: "12",
+                    slamdunks: "7"
+                },
+                "Brook Lopez": {
+                    number: "11",
+                    shoe: "17",
+                    points: "17",
+                    rebounds: "19",
+                    assists: "10",
+                    steals: "3",
+                    blocks: "1",
+                    slamdunks: "15"
+                
+                },
+                "Mason Plumlee": {
+                    number: "1",
+                    shoe: "19",
+                    points: "26",
+                    rebounds: "12",
+                    assists: "6",
+                    steals: "3",
+                    blocks: "8",
+                    slamdunks: "5"
+                },
+                "Jason Terry": {
+                    number: "31",
+                    shoe: "15",
+                    points: "19",
+                    rebounds: "2",
+                    assists: "2",
+                    steals: "4",
+                    blocks: "11",
+                    slamdunks: "1"
+                }
+            
             }
-           
 
-        ]
-        
-        
-    }
-    away: {
-
-    }
+        },
+        away: {}
+    } //end of meow variable
 }
-console.log(gameObject)
+console.log(gameObject())
