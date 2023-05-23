8f661cf17a8076c00f4313db4e032381fc7bfeb0
Kevinmrozek <kmrozek0859@gmail.com>
2023-05-23 14:33:31 -0500

    stuff

---
+++ src/00-objectball.js
@@ -0,0 +1,27 @@
+function gameObject() {
+    let playersName = ['Alan Anderson', 'Reggie Evans', 'Brook Lopez', 'Mason Plumlee', 'Jason Terry']
+    home: {
+        teamName: ''
+        colors: ['']
+        players: [
+            playersName = {
+                number: '',
+                shoe:'',
+                points:'',
+                rebounds:'',
+                assists:'',
+                steals:'',
+                blocks:'',
+                slamDunks:''
+            }
+           
+
+        ]
+        
+        
+    }
+    away: {
+
+    }
+}
+console.log(gameObject)
